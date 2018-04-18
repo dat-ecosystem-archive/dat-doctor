6abf9cbc784ccf5342924b8f36e470d21b7bebe8
Joe Hand <joe@hand.email>
2018-04-18 11:42:11 -0700

    add whoami test and more updates

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ var neatLog = require('neat-log')
 var output = require('neat-log/output')
 var neatTasks = require('neat-tasks')
 var chalk = require('chalk')
+var Menu = require('menu-string')
 var debug = require('debug')('dat-doctor')
+var runPublicTest = require('./lib/public-test')
+var whoamiTest = require('./lib/whoami-test')
 
 var DOCTOR_URL = 'doctor1.publicbits.org'
 var NODE_VER = process.version
@@ -20,6 +23,17 @@ module.exports = function (opts) {
 
   var tasks = [
     {
+      title: 'Who am I?',
+      task: function (state, bus, done) {
+        state.port = port
+        bus.on('error', function (err) {
+          if (!state.output) state.output = '  ' + chalk.dim(err)
+          else state.output += '\n  ' + chalk.dim(err)
+        })
+        whoamiTest(state, bus, done)
+      }
+    },
+    {
       title: 'Pinging the Dat Doctor',
       task: dnsLookupTask
     },
@@ -28,8 +42,30 @@ module.exports = function (opts) {
       task: nativeModuleTask
     },
     {
-      title: 'Checking Dat Public Connections',
-      task: publicPeerTask
+      title: 'Checking Dat Public Connections via TCP',
+      task: function (state, bus, done) {
+        publicPeerTask(state, bus, {tcp: true, utp: false}, done)
+      },
+      skip: function (cb) {
+        if (doctorAddress) return cb()
+        cb(`Skipping... unable to reach ${DOCTOR_URL}`)
+      }
+    },
+    {
+      title: 'Checking Dat Public Connections via UTP',
+      task: function (state, bus, done) {
+        publicPeerTask(state, bus, {tcp: false, utp: true}, done)
+      },
+      skip: function (cb) {
+        if (doctorAddress) return cb()
+        cb(`Skipping... unable to reach ${DOCTOR_URL}`)
+      }
+    },
+    {
+      title: 'Starting optional peer-to-peer test',
+      task: function (state, bus, done) {
+        done('todo')
+      }
     }
   ]
 
@@ -39,6 +75,10 @@ module.exports = function (opts) {
   var neat = neatLog([headerOutput, versionsOutput, runTasks.view])
   neat.use(getVersions)
   neat.use(runTasks.use)
+  // neat.input.on('update', function () {
+  //   neat.render()
+  // })
+
 
   function headerOutput (state) {
     return `Welcome to ${chalk.green('Dat')} Doctor!\n`
@@ -83,9 +123,10 @@ module.exports = function (opts) {
   function dnsLookupTask (state, bus, done) {
     dns.lookup(DOCTOR_URL, function (err, address, _) {
       if (err) {
-        return done(`Could not resolve ${DOCTOR_URL}`)
+        state.title = 'Unable to reach the Dat Doctor Server'
+        return done(`Please check if you can resolve the url manually, ${chalk.reset.cyanBright(`ping ${DOCTOR_URL}`)}`)
       }
-      state.title = 'Resolved Dat Doctor Address'
+      state.title = 'Resolved Dat Doctor Server'
       doctorAddress = address
       done()
     })
@@ -93,41 +134,29 @@ module.exports = function (opts) {
 
   function nativeModuleTask (state, bus, done) {
     try {
-      require('utp-native2')
+      require('utp-native')
       state.title = 'Loaded native modules'
     } catch (err) {
       state.title = 'Error loading native modules'
+      // TODO: link to FAQ/More Help
       return done(`Unable to load utp-native.\n  This will make it harder to connect peer-to-peer.`)
     }
     done()
   }
 
-  function publicPeerTask (state, bus, done) {
-    setTimeout(done, 5000)
-  }
+  function publicPeerTask (state, bus, opts, done) {
+    opts = Object.assign({port: port, address: doctorAddress}, opts)
+    state.errors = []
+    state.messages = []
 
-  function startPublicPeer (address, cb) {
-    var tests = [
-      {
-        name: 'utp only',
-        utp: true,
-        tcp: false,
-        port: 3282
-      },
-      {
-        name: 'tcp only',
-        utp: false,
-        tcp: true,
-        port: 3283
-      }
-    ]
-    var pending = tests.length
-    tests.forEach(function (test) {
-      test.address = address
-      runPublicPeerTest(test, function (err) {
-        if (err) return cb(err)
-        if (!--pending) cb()
-      })
+    bus.on('error', (err) => {
+      // TODO: persist these after task is done?
+      debug('ERROR - ', err)
+    })
+
+    runPublicTest(state, bus, opts, function (err) {
+      if (err) return done(err)
+      done()
     })
   }
 }
